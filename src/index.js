a25b4f346f8dcb5e2b111c7bd21ee4db481125cc
Divya <thakurdivya220703@gmail.com>
2025-02-14 09:30:56 +0530

    Add cookie-parser and cors middleware to enhance request handling

--- src/index.js
+++ src/index.js
@@ -10,7 +10,19 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(() => {
+  app.on("error", (error) => {
+            console.log("ERRR:", error);
+             throw error
+        })
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running at port: ${process.env.PORT}`);
+  })
+})
+.catch((err) => {
+  console.log("MONGO DB connection failed !!!!")
+})
 
 
 
